b4f6299f2df51275ecf83b5efac9dc7de255f9b5
vinipaulin099 <viniciusvpaulino@gmail.com>
2026-08-07 14:53:44 -0300

    Delete store-config.js

--- store-config.js
+++
@@ -1,6 +0,0 @@
-window.ITAJAO_STORE_CONFIG = Object.freeze({
-  functionsBaseUrl: 'https://eumgdopgiffzpahzcdsq.supabase.co/functions/v1',
-  supabasePublishableKey: 'sb_publishable_ydNEXUWmA65ZnPFXnpImvA_HMS',
-  // Só mude para true depois dos testes de pagamento e frete em produção.
-  commerceCheckoutEnabled: false
-});
